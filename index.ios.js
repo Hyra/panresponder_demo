6b3f87d1932f6467fd4939f87e7a9273230b1152
Stef van den Ham <stef@mindthecode.com>
2016-04-20 13:16:16 +0200

    Pan responder

--- index.ios.js
+++ index.ios.js
@@ -10,17 +10,59 @@ import React, {
   StyleSheet,
   Text,
   View,
-  Image // we want to use an image
+  Image, // we want to use an image
+  PanResponder, // we want to bring in the PanResponder system
+  Animated // we wil be using animated value
 } from 'react-native';
 
 class panresponder_demo extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pan: new Animated.ValueXY()
+    };
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      // When we drag/pan the object, set the delate to the states pan position
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+      }
+    });
+  }
+
   render() {
+    // Destructure the value of pan from the state
+    let { pan } = this.state;
+
+    // Calculate the x and y transform from the pan value
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    // Calculate the transform property and set it as a value for our style which we add below to the Animated.View component
+    let imageStyle = {transform: [{translateX}, {translateY}]};
+
     return (
       <View style={styles.container}>
-        <Image source={require('./assets/panresponder.png')} />
+        <Animated.View style={imageStyle} {...this._panResponder.panHandlers}>
+          <Image source={require('./assets/panresponder.png')} />
+        </Animated.View>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
